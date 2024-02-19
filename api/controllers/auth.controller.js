e164740df336373f12a23ded2602a9fe4dda4bbe
Abhishek Patial. <abhishekpl8219@gmailom>
2024-02-19 18:43:57 +0530

    Add signup and signin functionality.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
@@ -12,4 +14,22 @@ export const signup = async (req, res,next) => {
   } catch (error) {
     next(error);
   }
+};
+export const signin = async(req,res,next) => {
+
+  const {email ,password} = req.body;
+    try {
+      const validUser = await User.findOne({email});
+      if(!validUser) return next(errorHandler(404,'user not found'));
+      const validPasword =  bcryptjs.compareSync(password,validUser.password);
+      if(!validUser) return next(errorHandler(401,'password is in correct found'));
+      const token = jwt.sign({ id: validUser._id }, "xcv");
+      
+      res.cookie('access-token',token,{httpOnly:true}).status(200).json(validUser._doc);
+      
+
+    } catch (error) {
+      next(error);
+      
+    }
 };
--- api/index.js
+++ api/index.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+dotenv.config();
 mongoose.connect("mongodb+srv://royalrappy:royalrappy@mernestate.fnfxubk.mongodb.net/mern-estate?retryWrites=true&w=majority").then(()=>{
     console.log("Connected to Mongo DB!")
 }).catch((err)=>{
     console.log(err);
 })
+
+// Use this after the variable declaration
 const app = express();
 app.use(express.json());
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
     }
 );
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+app.use(cors(corsOptions)) 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use((err, req, res, next) => {
