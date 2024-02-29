0ca39baffd29ce292817a8390cb01abd41478e0b
Abhishek Patial. <abhishekpl8219@gmailom>
2024-02-29 23:31:00 +0530

    create delete user functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { vToken } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
 router.post('/create',vToken,createListing);
-
+router.delete('/delete/:id',vToken,deleteListing);
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -127,6 +127,24 @@ export default function Profile() {
     }
 
   };
+  const handleListingDelete =  async(listingId) => {
+    try {
+    
+      const res = await fetch(`http://localhost:3000/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev)=>prev.filter((listing)=>listing._id!==listingId));
+      
+    } catch (error) {
+      console.log(error.message);
+    }
+
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -215,7 +233,7 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 uppercase ' >
+                <button className='text-red-700 uppercase ' onClick={()=> handleListingDelete(listing._id) }>
                   Delete
                 </button>
                 <button className='text-green-700 uppercase '>
