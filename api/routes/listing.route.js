c1e00eb2004ca08d05bd79c814e31a5d1caad5c4
Abhishek Patial. <abhishekpl8219@gmailom>
2024-03-01 10:51:03 +0530

    Create update lisitng user api

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { vToken } from "../controllers/auth.controller.js";
 
@@ -7,4 +7,5 @@ import { vToken } from "../controllers/auth.controller.js";
 const router = express.Router();
 router.post('/create',vToken,createListing);
 router.delete('/delete/:id',vToken,deleteListing);
+router.post('/update/:id',vToken,updateListing);
 export default router;
